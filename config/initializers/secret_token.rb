88c51f038d6f545086af778298ff590c6203f31a
github username <github email address>
2013-11-04 10:01:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollywogRanchRailsRuby003::Application.config.secret_key_base = '09af72bd39dbfda469edde497ceb9bd4961c4fa7e6a015efb3699f5293359f49a471822eb0d51ca0453cbfc8b887c813bac109924c4565c67a41a7eca8646250'
